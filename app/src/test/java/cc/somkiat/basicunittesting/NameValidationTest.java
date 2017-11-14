fa304a7b63119959e93fc804f84a8e662684f05e
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-11-15 05:30:54 +0700

    Use @Before for reduce Smell Code

--- app/src/test/java/cc/somkiat/basicunittesting/NameValidationTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/NameValidationTest.java
@@ -1,56 +1,57 @@
 package cc.somkiat.basicunittesting;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class NameValidationTest {
+    NameValidation nameValidation;
+
+    @Before
+    public void setup(){
+        nameValidation = new NameValidation();
+    }
+
     @Test
     public void caseEmpty(){
-        NameValidation nameValidation = new NameValidation();
         boolean result = nameValidation.isEmpty("");
         assertTrue("", result);
     }
 
     @Test
     public void isNull(){
-        NameValidation nameValidation = new NameValidation();
         boolean result = nameValidation.isNull(null);
         assertTrue("", result);
     }
 
     @Test
     public void isNotNull(){
-        NameValidation nameValidation = new NameValidation();
         boolean result = nameValidation.isNull("Hello");
         assertFalse("", result);
     }
 
     @Test
     public void lengthBetweenTwoAndTwenty(){
-        NameValidation nameValidation = new NameValidation();
         boolean result = nameValidation.checkLength("Somkiat");
         assertTrue("", result);
     }
 
     @Test
     public void lengthLessThanTwo(){
-        NameValidation nameValidation = new NameValidation();
         boolean result = nameValidation.checkLength("s");
         assertFalse("Name Length less than 2 char.", result);
     }
 
     @Test
     public void lengthMoreThanTwo(){
-        NameValidation nameValidation = new NameValidation();
         boolean result = nameValidation.checkLength("abcdefghijklmnopabcdefghtjklmno");
         assertFalse("Name Length less than 2 char.", result);
     }
 
     @Test
     public void nameContainAlphabet(){
-        NameValidation nameValidation = new NameValidation();
         boolean result = nameValidation.containAlphabet("Somkiat");
         assertTrue("", result);
     }
