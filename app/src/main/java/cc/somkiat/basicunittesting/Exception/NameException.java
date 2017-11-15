edc475a9ec6ff1a1d843a771ba20dd765f3f4ffd
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-11-15 20:22:18 +0700

    Create class NameException

---
+++ app/src/main/java/cc/somkiat/basicunittesting/Exception/NameException.java
@@ -0,0 +1,11 @@
+package cc.somkiat.basicunittesting.Exception;
+
+/**
+ * Created by nutchy on 15/11/2017 AD.
+ */
+
+public class NameException extends Exception {
+    public NameException(String message) {
+        super(message);
+    }
+}
