ea19a1766c68d4aeb7d7605afe4a80de224b6897
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-11-15 20:21:30 +0700

    Create class EmailException

---
+++ app/src/main/java/cc/somkiat/basicunittesting/Exception/EmailException.java
@@ -0,0 +1,11 @@
+package cc.somkiat.basicunittesting.Exception;
+
+/**
+ * Created by nutchy on 15/11/2017 AD.
+ */
+
+public class EmailException extends Exception {
+    public EmailException(String message) {
+        super(message);
+    }
+}
