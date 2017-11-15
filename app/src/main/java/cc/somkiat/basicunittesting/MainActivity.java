0042dc21f0aa25df9b73e6158eeb18051dbbf528
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-11-16 03:46:50 +0700

    Implement Validation into MainActivity

--- app/src/main/java/cc/somkiat/basicunittesting/MainActivity.java
+++ app/src/main/java/cc/somkiat/basicunittesting/MainActivity.java
@@ -3,20 +3,48 @@ package cc.somkiat.basicunittesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import cc.somkiat.basicunittesting.Model.ResultValidation;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText etEmail, etUsername;
+    private String email, userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etEmail = findViewById(R.id.emailInput);
+        etUsername = findViewById(R.id.userNameInput);
     }
 
+
     public void onSaveClick(View view) {
-        //TODO
+        email = etEmail.getText().toString();
+        userName = etUsername.getText().toString();
+
+        NameValidation nameValidation = new NameValidation();
+        EmailValidation emailValidation = new EmailValidation();
+
+        ResultValidation nameValidationResult = nameValidation.validate(userName);
+        ResultValidation emailValidationResult = emailValidation.validate(email);
+
+        System.out.println(nameValidationResult.getResult());
+        System.out.println(emailValidationResult.getResult());
+
+        if(!nameValidationResult.getResult()) {
+            Toast.makeText(this, nameValidationResult.getMessage(), Toast.LENGTH_SHORT).show();
+        } else if (!emailValidationResult.getResult()) {
+            Toast.makeText(this, emailValidationResult.getMessage(), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Save Successful", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onRevertClick(View view) {
-        //TODO
+        etUsername.setText("");
+        etEmail.setText("");
     }
 }
