eb0a2a1c90a367e33b15a02d0b385dbc845664be
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-11-15 20:26:39 +0700

    Create User's Model

---
+++ app/src/main/java/cc/somkiat/basicunittesting/Model/User.java
@@ -0,0 +1,25 @@
+package cc.somkiat.basicunittesting.Model;
+
+/**
+ * Created by nutchy on 15/11/2017 AD.
+ */
+
+public class User {
+    private String email, name;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
