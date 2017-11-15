a17724afc742f5bb7da5b30560ed81c527fe7c70
Chayanon Thongpila <58070021@kmitl.ac.th>
2017-11-16 04:01:58 +0700

    UI Test

---
+++ app/src/androidTest/java/cc/somkiat/basicunittesting/MainActivityTest.java
@@ -0,0 +1,49 @@
+package cc.somkiat.basicunittesting;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void saveSuccess() {
+        onView(withId(R.id.userNameInput)).perform(replaceText("somkiat"), closeSoftKeyboard());
+        onView(withId(R.id.emailInput)).perform(scrollTo(), replaceText("abc"), closeSoftKeyboard());
+        onView(withId(R.id.saveButton)).perform(scrollTo(), click());
+    }
+
+    
+
+}
